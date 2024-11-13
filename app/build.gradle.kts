5daf0672c6ad059b5eb78b8c51b2c01aa89041ab
Ricardo <ricardo.rco.15@gmail.com>
2024-11-12 21:22:50 -0300

    testando merge

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.barbeariadomarquinho"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.barbeariadomarquinho"
